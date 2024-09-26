040d1a7f508c191836220b52c293b3e03ce01836
Marcelo Barboza <26288817+mpbsd@users.noreply.github.com>
2024-09-26 08:59:25 -0300

    add functions to specify a set from its elements

--- src/main.c
+++ src/main.c
@@ -8,6 +8,7 @@ int binomial(int, int);
 int poweroftwo(int);
 void allocate(int ****);
 void initialize(int ***);
+void elements(int ***, int, int);
 void display(int ***);
 
 int main(void) {
@@ -75,22 +76,20 @@ void initialize(int ***powerset) { /*{{{*/
 
   powerset[0][0][0] = '*';
 
-  for (i = 1; i < N; ++i) {
-    if (i == 1) {
-      for (j = 0; j < N; ++j) {
-        powerset[1][j][0] = j;
-      }
-    } else {
-      p = binomial(N, i - 1);
-      j = 0;
-      for (k = 0; k < p; ++k) {
-        if (powerset[i - 1][k][i - 2] < N - 1) {
-          for (l = powerset[i - 1][k][i - 2] + 1; l < N; ++l) {
-            for (m = 0; m < i - 1; ++m) {
-              powerset[i][j][m] = powerset[i - 1][k][m];
-            }
-            powerset[i][j++][i - 1] = l;
+  for (j = 0; j < N; ++j) {
+    powerset[1][j][0] = j;
+  }
+
+  for (i = 2; i < N; ++i) {
+    p = binomial(N, i - 1);
+    j = 0;
+    for (k = 0; k < p; ++k) {
+      if (powerset[i - 1][k][i - 2] < N - 1) {
+        for (l = powerset[i - 1][k][i - 2] + 1; l < N; ++l) {
+          for (m = 0; m < i - 1; ++m) {
+            powerset[i][j][m] = powerset[i - 1][k][m];
           }
+          powerset[i][j++][i - 1] = l;
         }
       }
     }
@@ -101,33 +100,32 @@ void initialize(int ***powerset) { /*{{{*/
   }
 } /*}}}*/
 
+void elements(int ***powerset, int i, int j) { /*{{{*/
+  int k = 0;
+
+  if (i == 0) {
+    printf("*");
+  } else {
+    for (k = 0; k < i; ++k) {
+      printf("%s%d", (k == 0) ? "{" : ", ", powerset[i][j][k]);
+    }
+    printf("}");
+  }
+} /*}}}*/
+
 void display(int ***powerset) { /*{{{*/
   int i = 0;
   int j = 0;
-  int k = 0;
   int p = 0;
 
-  printf("\n0: *\n");
+  printf("\n");
 
-  for (i = 1; i <= N; ++i) {
+  for (i = 0; i <= N; ++i) {
     printf("%d: ", i);
     p = binomial(N, i);
     for (j = 0; j < p; ++j) {
-      if (i == 1) {
-        printf("{%d}", powerset[i][j][0]);
-      } else {
-        for (k = 0; k < i; ++k) {
-          if (k == 0) {
-            printf("{%d", powerset[i][j][k]);
-          } else if (k == i - 1) {
-            printf(", %d}", powerset[i][j][k]);
-          } else {
-            printf(", %d", powerset[i][j][k]);
-          }
-        }
-      }
+      elements(powerset, i, j);
       printf("%s", (j < p - 1) ? ", " : "\n");
     }
   }
-
 } /*}}}*/
